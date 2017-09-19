2c834732440a7535966247d23a0e69c09c4c74c6
zohoMatt <zuoh14@mails.tsinghua.edu.cn>
2017-09-19 10:47:26 +0800

    refactor: Clean code

--- src/localSeach.js
+++ src/localSeach.js
@@ -6,24 +6,20 @@ const MEDIA_EXTENSIONS = require('../configs/MediaExtensions').extname
 /******************** Helpers level 0 *******************/
 let isDir = fullPath => fs.existsSync(fullPath) && fs.lstatSync(fullPath).isDirectory()
 let readFullPath = dirPath => fs.readdirSync(dirPath).map(f => path.join(dirPath, f))
+let isMedia = filePath => MEDIA_EXTENSIONS.includes(path.extname(filePath).slice(1))
 
 isDir = curry(isDir)
-
-/******************** Helpers level 1 *******************/
-let listRecursively = start => {
-    if (!isDir(start)) return start
-    return chain(listRecursively, readFullPath(start))
-}
-
-listRecursively = curry(listRecursively)
-/******************** Features *******************/
-let listAllFiles = listRecursively
-
-let isMedia = filePath => MEDIA_EXTENSIONS.includes(path.extname(filePath).slice(1))
+readFullPath = curry(readFullPath)
 isMedia = curry(isMedia)
+
+/******************** Features *******************/
+let listAllFiles = start => {
+    if (!isDir(start)) return start
+    return chain(listAllFiles, readFullPath(start))
+}
 
 let listMediaFiles = compose(filter(isMedia), listAllFiles)
 
 /******************** Exports *******************/
-exports.listAllFiles = listAllFiles
-exports.listMediaFiles = listMediaFiles
+exports.listAllFiles = curry(listAllFiles)
+exports.listMediaFiles = curry(listMediaFiles)
