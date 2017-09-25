baccbd6101fd752efcc9a4abdcc4e7c4b16f6645
zohoMatt <zuoh14@mails.tsinghua.edu.cn>
2017-09-25 18:00:25 +0800

    feat: Adjust configs

---
+++ configs/Extensions.js
@@ -0,0 +1,18 @@
+module.exports = {
+    media: [
+        '.flv',
+        '.mkv', '.mp4', '.mpg', '.mpeg', '.m4v', '.mov',
+        '.rmvb', '.rm',
+        '.wmv'
+    ],
+    subtitle: [
+        '.ass',
+        '.srt',
+        '.sub'
+    ],
+    archive: [
+        '.7z', '.7zip',
+        '.rar',
+        '.zip'
+    ]
+}
--- configs/MediaExtensions.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    extname: [
-        'flv',
-        'mkv', 'mp4', 'mpg', 'mpeg', 'm4v', 'mov',
-        'rmvb', 'rm',
-        'wmv'
-    ]
-}
